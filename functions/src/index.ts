087dfda02925d6fe5d22f2fccb88906a89c50f2e
Castle Kerr <castlekerr@hotmail.com>
2021-04-08 17:25:37 +0530

    Added ability to upload to Google Forms

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,6 +5,7 @@ import * as engines from "consolidate";
 import * as recaptcha from "express-recaptcha";
 import {RecaptchaResponseV3} from "express-recaptcha/dist/interfaces";
 import {body, validationResult, matchedData, Result} from "express-validator";
+import * as unirest from "unirest";
 import * as quoteRequestTemplate from "./quote_request_email_template.json";
 
 const recaptchaVerify: express.Handler = new recaptcha.RecaptchaV3(
@@ -19,6 +20,15 @@ admin.firestore().collection("mail_templates")
     .doc(quoteRequestTemplateId)
     .set(quoteRequestTemplate);
 
+type Data = {
+  name: string,
+  email: string,
+  phone: string,
+  description: string,
+  score: number,
+  timestamp: FirebaseFirestore.Timestamp,
+};
+
 const submitQuote: express.Handler = async (request, response, next) => {
   // Validate request
   const result = validationResult(request);
@@ -30,20 +40,51 @@ const submitQuote: express.Handler = async (request, response, next) => {
   const {name, email, phone, description} = matchedData(request);
   const score = (request.recaptcha as RecaptchaResponseV3).data?.score ?? 0.0;
   const timestamp = admin.firestore.Timestamp.now();
-  const data = {name, email, phone, description, score, timestamp};
+  const data: Data = {name, email, phone, description, score, timestamp};
 
-  // If the score indicates that the request is likely spam, don't save it
+  // If the score indicates that the request is likely spam, don't email it
   if (score > 0.5) {
-    await admin.firestore().collection("quote_requests").add(data);
+    await sendEmail(data);
   }
 
-  await admin.firestore().collection("mail").add({
-    toUids: ["quote_requests_to_user"],
-    replyTo: email,
-    template: {name: quoteRequestTemplateId, data},
-  });
+  await saveToDatabase(data);
+  await saveToGoogleForm(data);
 
   return response.render("success");
+};
+
+const sendEmail = async (data: Data): Promise<void> => {
+  await admin.firestore().collection("mail").add({
+    toUids: ["quote_requests_to_user"],
+    replyTo: data.email,
+    template: {name: quoteRequestTemplateId, data},
+  });
+};
+
+const saveToDatabase = async (data: Data): Promise<void> => {
+  await admin.firestore().collection("quote_requests").add(data);
+};
+
+const saveToGoogleForm = async (data: Data): Promise<void> => {
+  const gForm = await admin
+      .firestore()
+      .doc("google_forms/quote_request")
+      .get()
+      .then((doc) => doc.data());
+
+  if (!gForm) {
+    return;
+  }
+
+  const url = `https://docs.google.com/forms/u/2/d/e/${gForm.id}/formResponse`;
+  await unirest("POST", url)
+      .form({
+        [gForm.fieldIds.name]: data.name,
+        [gForm.fieldIds.email]: data.email,
+        [gForm.fieldIds.phone]: data.phone,
+        [gForm.fieldIds.description]: data.description,
+        [gForm.fieldIds.score]: data.score,
+      });
 };
 
 const app = express();
