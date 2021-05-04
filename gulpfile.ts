08fc857fc8fe2db7f215f727b0ef2533ed06709f
Castle Kerr <castlekerr@hotmail.com>
2021-05-04 10:20:52 +0530

    Renamed public folder to static

--- gulpfile.ts
+++ gulpfile.ts
@@ -3,13 +3,13 @@ import * as clean from 'gulp-clean';
 import * as handlebars from 'gulp-compile-handlebars';
 import * as rename from 'gulp-rename';
 
-task('clean', () => src('./public/*.html')
+task('clean', () => src('./static/*.html')
     .pipe(clean()));
 
 task('html', () => src('./src/pages/*')
     .pipe(handlebars({}, { batch: './src/partials' }))
     .pipe(rename({ extname: '.html' }))
-    .pipe(dest('./public')));
+    .pipe(dest('./static')));
 
 task('default', series('clean', 'html'));
   
