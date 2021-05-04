0cdb363935a69928f5c447f3ea9ea8b12961c0ba
Castle Kerr <castlekerr@hotmail.com>
2021-05-05 02:46:34 +0530

    Used npm for libraries

--- gulpfile.ts
+++ gulpfile.ts
@@ -3,18 +3,37 @@ import * as clean from 'gulp-clean';
 import * as sass from 'gulp-sass';
 import * as handlebars from 'gulp-compile-handlebars';
 import * as rename from 'gulp-rename';
+import * as dotenv from 'dotenv';
 
-task('clean', () => src(['./static/*.html', './static/css/*'])
+dotenv.config();
+
+task('clean', () => src([
+        './static/*.html',
+        './static/css/*',
+        './static/images/photoswipe/',
+        './static/js/parallax.min.js',
+    ], {allowEmpty: true})
     .pipe(clean()));
 
 task('sass', () => src('./src/styles/*')
     .pipe(sass().on('error', sass.logError))
     .pipe(dest('./static/css')));
 
-task('hbs', () => src('./src/pages/*')
-    .pipe(handlebars({}, { batch: './src/pages/partials' }))
+task('hbs', () => src(['./src/pages/*', '!./src/pages/partials'])
+    .pipe(handlebars(process.env, { batch: './src/pages/partials' }))
     .pipe(rename({ extname: '.html' }))
     .pipe(dest('./static')));
 
-task('default', series('clean', parallel('sass', 'hbs')));
+task('photoswipe', () => src([
+        './node_modules/photoswipe/dist/photoswipe-ui-default.min.js',
+        './node_modules/photoswipe/dist/photoswipe.min.js',
+        './node_modules/photoswipe/dist/photoswipe.css',
+        './node_modules/photoswipe/dist/*default-skin/**/*',
+    ])
+    .pipe(dest('./static/images/photoswipe/')));
+
+task('parallax', () => src('./node_modules/jquery-parallax.js/parallax.min.js')
+    .pipe(dest('./static/js')))
+
+task('default', series('clean', parallel('sass', 'hbs', 'photoswipe', 'parallax')));
   
