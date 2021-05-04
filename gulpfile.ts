28925458d26d5e89af781134fd21a5b083340076
Castle Kerr <castlekerr@hotmail.com>
2021-05-04 18:40:50 +0530

    Added SASS

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,15 +1,20 @@
-import { task, src, dest, series } from 'gulp';
+import { task, src, dest, series, parallel, watch } from 'gulp';
 import * as clean from 'gulp-clean';
+import * as sass from 'gulp-sass';
 import * as handlebars from 'gulp-compile-handlebars';
 import * as rename from 'gulp-rename';
 
-task('clean', () => src('./static/*.html')
+task('clean', () => src(['./static/*.html', './static/css/*'])
     .pipe(clean()));
 
-task('html', () => src('./src/pages/*')
-    .pipe(handlebars({}, { batch: './src/partials' }))
+task('sass', () => src('./src/styles/*')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(dest('./static/css')));
+
+task('hbs', () => src('./src/pages/*')
+    .pipe(handlebars({}, { batch: './src/pages/partials' }))
     .pipe(rename({ extname: '.html' }))
     .pipe(dest('./static')));
 
-task('default', series('clean', 'html'));
+task('default', series('clean', parallel('sass', 'hbs')));
   
